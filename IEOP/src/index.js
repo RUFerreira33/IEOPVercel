4e103039e14cbfbf05d3817d9494799d18325e5c
RUFerreira33 <r.u.ferreira@ipvc.pt>
2026-01-02 13:40:07 +0000

    index.js funcional2

--- IEOP/src/index.js
+++ IEOP/src/index.js
@@ -3,40 +3,29 @@ import cors from "cors";
 import dotenv from "dotenv";
 
 import { requireInternalKey } from "./middleware/auth.middleware.js";
-import productsRoutes from "./routes/products.routes.js";
-import ordersRoutes from "./routes/orders.routes.js";
-import documentsRoutes from "./routes/documents.routes.js";
 import customersRoutes from "./routes/customers.routes.js";
+import ordersRoutes from "./routes/orders.routes.js";
+import productsRoutes from "./routes/products.routes.js";
+import documentsRoutes from "./routes/documents.routes.js";
 
 dotenv.config();
 
 const app = express();
 
 app.use(express.json());
-app.use(
-  cors({
-    origin: process.env.CORS_ORIGIN || "*",
-    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "x-api-key", "Authorization"]
-  })
-);
+app.use(cors());
 
-// Público
+// público
 app.get("/health", (req, res) => {
   res.json({ ok: true, status: "API a funcionar" });
 });
 
-// Protegido (PowerApps/cliente interno)
+// protegido
 app.use(requireInternalKey);
 
-app.use("/products", productsRoutes);
-app.use("/orders", ordersRoutes);
-app.use("/documents", documentsRoutes);
 app.use("/customers", customersRoutes);
-
-if (process.env.VERCEL !== "1") {
-  const PORT = process.env.PORT || 5000;
-  app.listen(PORT, () => console.log(`Local: http://localhost:${PORT}`));
-}
+app.use("/orders", ordersRoutes);
+app.use("/products", productsRoutes);
+app.use("/documents", documentsRoutes);
 
 export default app;
